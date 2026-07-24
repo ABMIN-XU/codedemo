884cb72038f1fb59d8df030908b3186d973aa32b
ABMIN-XU <3574982241@qq.com>
2026-07-24 16:05:07 +0800

    新增链表代码 list.c

---
+++ list.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+    int data;
+    struct node *next;
+}node;
+node* initlist(){
+    node *head=(node*)malloc(sizeof(node));
+    head->next=NULL;
+    head->data=0;
+    return head;
+}
+void insert1(node*l,int e){
+    node *p=(node*)malloc(sizeof(node));
+    p->data=e;
+    p->next=l->next;
+    l->next=p;
+}
+void listnode(node* l){
+    node *p=l->next;
+    while(p!=NULL){
+        printf("%d ",p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+node* get_tail(node*l){
+    node *p=l;
+    while(p->next!=NULL){
+        p=p->next;
+    }
+    return p;
+}
+node* insert2(node*l,int e){
+    node *p=(node*)malloc(sizeof(node));
+    p->data=e;
+    p->next=l->next;
+    l->next=p;
+    return p;
+}
+int main(){
+    node *l=initlist();
+    node *tail=get_tail(l);
+    tail=insert2(tail,1);
+    tail=insert2(tail,2);
+    tail=insert2(tail,3);
+    listnode(l);
+    return 0;
+}
